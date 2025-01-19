8203bbc29539332ecfc599fd135bd46669992fdc
Karinaikhwani <karinaikhwaaa@gmail.com>
2025-01-19 21:51:51 +0700

    Add files via upload

---
+++ CafeCashier.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cafecashier;
+
+/**
+ *
+ * @author karina
+ */
+public class CafeCashier {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new PesananCafe().show();
+        // TODO code application logic here
+    }
+    
+}
